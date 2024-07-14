373c13dbf9c373ecf5d1919b72a80337c5fddc2e
Md. Salauddin <salauddinahmed555@gmail.com>
2024-07-15 02:26:35 +0600

    changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Md. Salauddins Portfolio",
+  description: "Modern & Minimal Portfolio",
 };
 
 export default function RootLayout({
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" className="rounded-full" href="IMG_20230428_203608.jpg" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
