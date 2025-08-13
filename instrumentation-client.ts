378964953d38bbf4a745d89807cab5fb7b001223
Md. Salauddin <salauddinahmed555@gmail.com>
2025-08-14 03:05:13 +0600

    trying to fix sentry sdk outdated and other issues one year later touched the project rework

---
+++ instrumentation-client.ts
@@ -0,0 +1,17 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3ea1bf0f19d6ceea58fec401f7b45b0e@o4507544004853760.ingest.de.sentry.io/4507544018223184",
+
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  integrations: [],
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
+
+// This export will instrument router navigations, and is only relevant if you enable tracing.
+// `captureRouterTransitionStart` is available from SDK version 9.12.0 onwards
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
